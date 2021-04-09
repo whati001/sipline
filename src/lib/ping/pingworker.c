09910e44d67d72e4d2538360a7b73bc243bf4251
whati001 <andreas.karner@student.tugraz.at>
2021-04-09 22:33:30 +0200

    AK - added GET and POST support to ping worker

--- src/lib/ping/pingworker.c
+++ src/lib/ping/pingworker.c
@@ -4,7 +4,89 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <libnet.h>
 #include "ping/pingservice.h"
+
+int doPost(int sock, ping_task_t *task) {
+    size_t needed = snprintf(NULL, 0,
+                             "POST %s HTTP/1.1\r\nHost: %s\r\nContent-Type: application/json\r\nContent-Length: %ld\r\n\r\n%s\r\n\r\n",
+                             task->query, task->host, strlen(task->body), task->body);
+    char *buffer = (char *) calloc(sizeof(char), needed + 1);
+    sprintf(buffer,
+            "POST %s HTTP/1.1\r\nHost: %s\r\nContent-Type: application/json\r\nContent-Length: %ld\r\n\r\n%s\r\n\r\n",
+            task->query, task->host, strlen(task->body), task->body);
+
+    send(sock, buffer, strlen(buffer), 0);
+    fprintf(stdout, "POST request send to socket\n");
+
+    fflush(stdout);
+    free(buffer);
+    return EXIT_SUCCESS;
+}
+
+int doGet(int sock, ping_task_t *task) {
+    size_t needed = snprintf(NULL, 0,
+                             "GET %s HTTP/1.1\r\nHost: %s\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n",
+                             task->query, task->host);
+    char *buffer = (char *) calloc(sizeof(char), needed + 1);
+    sprintf(buffer,
+            "GET %s HTTP/1.1\r\nHost: %s\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n",
+            task->query, task->host);
+
+    send(sock, buffer, strlen(buffer), 0);
+    fprintf(stdout, "GET request send to socket\n");
+
+    fflush(stdout);
+    free(buffer);
+    return EXIT_SUCCESS;
+}
+
+
+int prepareSocket(int *sock, ping_task_t *task) {
+    int prepare_socket;
+    struct sockaddr_in serv_addr;
+    if ((prepare_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        fprintf(stdout, "Socket creation error \n");
+        return EXIT_FAILURE;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(task->port);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, task->host, &serv_addr.sin_addr) <= 0) {
+        fprintf(stdout, "Invalid address/ Address not supported \n");
+        return EXIT_FAILURE;
+    }
+
+    if (connect(prepare_socket, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        fprintf(stdout, "Connection failed\n");
+        return EXIT_FAILURE;
+    }
+
+    *sock = prepare_socket;
+    return EXIT_SUCCESS;
+}
+
+int pingBackend(ping_task_t *task) {
+    int sock;
+    if (EXIT_FAILURE == prepareSocket(&sock, task)) {
+        fprintf(stdout, "Failed to open socket, skip ping task\n");
+        return EXIT_FAILURE;
+    }
+
+    switch (task->method) {
+        case POST:
+            fprintf(stdout, "Connection established, send POST request\n");
+            return doPost(sock, task);
+        case GET:
+            fprintf(stdout, "Connection established, send GET request\n");
+            return doGet(sock, task);
+        default:
+            return EXIT_FAILURE;
+    }
+}
 
 void *workerRoutine(void *args) {
     ping_queue_t *queue = (ping_queue_t *) args;
@@ -18,9 +100,15 @@ void *workerRoutine(void *args) {
         if (EXIT_FAILURE == popPingTask(queue, &task)) {
             fprintf(stdout, "PingWorker failed to pop task, wait and hope for the next one\n");
         } else {
-            fprintf(stdout, "PingWorker finished task successfully\n");
             char *stringify_task = stringifyPingTask(task);
             fprintf(stdout, "%s\n", stringify_task);
+
+            if (EXIT_FAILURE == pingBackend(task)) {
+                fprintf(stdout, "PingWorker failed to ping backend\n");
+            } else {
+                fprintf(stdout, "PingWorker successfully pinged backend\n");
+            }
+
             free(stringify_task);
             destroyPingTask(task);
         }
--- tests/check_pingworker.c
+++ tests/check_pingworker.c
@@ -6,6 +6,15 @@
 #include <stdlib.h>
 #include <sipline.h>
 
+void wrapFuncInHttpServer(void (*func)(void)) {
+    printf("Start python http server at port 2711\n");
+    system("/usr/bin/tmux new-session -d -s \"pythonHttpServer\" python3 -m http.server 2711");
+    printf("Start python http server started at port 2711\n");
+    func();
+    printf("Kill python server at port 2711\n");
+    system("/usr/bin/tmux kill-session -t pythonHttpServer");
+}
+
 START_TEST(test_init_worker) {
     ck_assert_int_eq(EXIT_FAILURE, initPingService(NULL));
 
@@ -17,14 +26,72 @@ START_TEST(test_init_worker) {
 
 END_TEST
 
+START_TEST(test_worker_tcp_get) {
+    ping_task_t *task = (ping_task_t *) malloc(sizeof(ping_task_t));
+    *task = (ping_task_t) {
+            HTTP,
+            strdup("127.0.0.1"),
+            2711,
+            GET,
+            strdup("/sipbell/test"),
+            strdup("something"),
+            NULL
+    };
+
+    ck_assert_int_eq(EXIT_SUCCESS, pingBackend(task));
+    destroyPingTask(task);
+}
+
+END_TEST
+
+START_TEST(test_worker_tcp_post) {
+    ping_task_t *task = (ping_task_t *) malloc(sizeof(ping_task_t));
+    *task = (ping_task_t) {
+            HTTP,
+            strdup("127.0.0.1"),
+            2711,
+            POST,
+            strdup("/sipbell/test"),
+            strdup("{\"type\":0, \"from\":\"whati001\",\"to\":\"L12\"}"),
+            NULL
+    };
+
+    ck_assert_int_eq(EXIT_SUCCESS, pingBackend(task));
+    destroyPingTask(task);
+}
+
+END_TEST
+
+START_TEST(test_worker_tcp_fail_address) {
+    ping_task_t *task = (ping_task_t *) malloc(sizeof(ping_task_t));
+    *task = (ping_task_t) {
+            HTTP,
+            strdup("localhost"),
+            2711,
+            POST,
+            strdup("/sipbell/test"),
+            strdup("{\"type\":0, \"from\":\"whati001\",\"to\":\"L12\"}"),
+            NULL
+    };
+
+    ck_assert_int_eq(EXIT_FAILURE, pingBackend(task));
+    destroyPingTask(task);
+}
+
+END_TEST
+
 Suite *queue_suite() {
     Suite *s;
     TCase *tc_core;
 
+    // INFO: please start a http server before running those tests
     s = suite_create("PingWorker");
     tc_core = tcase_create("PingWorkerBasic");
 
     tcase_add_test(tc_core, test_init_worker);
+    tcase_add_test(tc_core, test_worker_tcp_get);
+    tcase_add_test(tc_core, test_worker_tcp_post);
+    tcase_add_test(tc_core, test_worker_tcp_fail_address);
     suite_add_tcase(s, tc_core);
 
     return s;
